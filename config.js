e9709cebcaa02fb8b7461b6f485dfaee7e13168b
Chris Mathew Aje <chrismaje63@gmail.com>
2025-09-06 11:55:14 +0530

    feat: update API_URL to use environment variable for better configuration management

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbzQUPd7_yU9mig6CeVB_CWCy4dLR-PXVQe4ybpX3X7lFHk8K4pWWefS35YJivFBfP7L/exec'
+    API_URL: import.meta.env.VITE_API_URL
 };
